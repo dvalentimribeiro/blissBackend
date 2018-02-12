3c875e107fbd9ff69dea39af369ce676c3db3bd6
Daniel Valentim Ribeiro <daniel.ribeiro@rtp.pt>
2018-02-12 11:39:54 +0000

    New changes

--- BlissRecruitment/Models/Question.cs
+++ BlissRecruitment/Models/Question.cs
@@ -11,7 +11,7 @@ namespace Bliss.Models
         public int id { get; set; }
         public string question { get; set; }
         public string image_url { get; set; }
-        public string thumb_url { ge t; set; }
+        public string thumb_url { get; set; }
         [Column(TypeName = "datetime2")]
         public DateTime published_at { get; set; }
         public virtual List<Choice> Choices { get; set; }
